4e7c23e95a2c169f16e7c32614cc58417acce757
Shirasaka-Takahiro <kenshirou913@gmail.com>
2024-05-26 21:48:48 +0900

    first commit

---
+++ module/acm.tf
@@ -0,0 +1,28 @@
+##ACM
+resource "aws_acm_certificate" "cert_alb" {
+  domain_name               = "*.${var.zone_name}"
+  subject_alternative_names = ["${var.zone_name}"]
+  validation_method         = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+##Create cname record for alb on Route53
+resource "aws_route53_record" "cert_validation_alb" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert_alb.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  type            = each.value.type
+  ttl             = "300"
+  zone_id         = var.zone_id
+}
